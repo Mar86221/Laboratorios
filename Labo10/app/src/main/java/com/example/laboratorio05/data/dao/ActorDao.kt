8d5ef42c59271b4a1071689f344c0730de0c7770
DiegoCruch <00131320@uca.edu.sv>
2023-05-30 12:19:07 -0600

    Labo10

---
+++ Labo10/app/src/main/java/com/example/laboratorio05/data/dao/ActorDao.kt
@@ -0,0 +1,19 @@
+package com.example.laboratorio05.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import com.example.laboratorio05.data.model.ActorModel
+
+@Dao
+interface ActorDao {
+    // TODO: create getAllActors method
+    @Query("SELECT * FROM ACTOR_TABLE")
+    suspend fun getAllActors(): List<ActorModel>
+
+    // TODO: create insertActor method
+    @Transaction
+    @Insert
+    suspend fun insertActor(actor: ActorModel)
+}
